0a142c6a67919217ff8f8447d180bcafad36ede2
B. Olivér <djjean18@gmail.com>
2015-11-23 20:51:03 +0100

    - create new package

--- src/DynamicFormAsset.php
+++ src/DynamicFormAsset.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * @link      https://github.com/wbraganca/yii2-dynamicform
+ * @link      https://github.com/bajadev/yii2-dynamicform
  * @copyright Copyright (c) 2014 Wanderson Bragança
- * @license   https://github.com/wbraganca/yii2-dynamicform/blob/master/LICENSE
+ * @license   https://github.com/bajadev/yii2-dynamicform/blob/master/LICENSE
  */
 
-namespace wbraganca\dynamicform;
+namespace bajadev\dynamicform;
 
 /**
  * Asset bundle for dynamicform Widget
