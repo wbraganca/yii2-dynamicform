1eec06f98a3989db07bdc0b90c13b3e0291885bb
Steve Simpson <steve@lcsas.us>
2016-07-24 00:39:05 -0400

    Fixed namespace for patched version.

--- src/DynamicFormAsset.php
+++ src/DynamicFormAsset.php
@@ -5,7 +5,7 @@
  * @license   https://github.com/wbraganca/yii2-dynamicform/blob/master/LICENSE
  */
 
-namespace wbraganca\dynamicform;
+namespace SteveSimpson\dynamicform;
 
 /**
  * Asset bundle for dynamicform Widget
