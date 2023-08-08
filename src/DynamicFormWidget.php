59e32c6e13f9a0bbe8f5179da534a252a6108206
Soares e Neves <soareseneves@terra.com.br>
2023-08-08 12:58:50 -0300

    Fix Issue #333
    Fix javascript error: Cannot read properties of undefined (reading 'settings')

--- src/DynamicFormWidget.php
+++ src/DynamicFormWidget.php
@@ -216,7 +216,7 @@ class DynamicFormWidget extends \yii\base\Widget
         $js .= "});\n";
         $view->registerJs($js, $view::POS_READY);
 
-        $js = 'jQuery("#' . $this->formId . '").yiiDynamicForm(' . $this->_hashVar .');' . "\n";
+        $js = 'jQuery("#' . $this->formId . '").on(\'afterInit\', function () {jQuery("#' . $this->formId . '").yiiDynamicForm(' . $this->_hashVar .');});' . "\n";
         $view->registerJs($js, $view::POS_LOAD);
     }
 
